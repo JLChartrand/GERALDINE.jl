b86b07f15ff5e5292c8fb0f98dbaa78ba343ec43
JLChartrand <jl_chartrand@hotmail.com>
2020-03-10 04:35:07 -0400

    .

--- src/BTR/BTR_BFGS.jl
+++ src/BTR/BTR_BFGS.jl
@@ -20,18 +20,51 @@ import Base.setindex!
 function setindex!(a::BFGS_Matrix, value, index...)
     setindex!(a.H, value, index...)
 end
+
+
+function btr_bfgs(f::Function, g!::Function, state::BTRState{Array{Float64,2}}, x0::Vector; 
+        verbose::Bool = true, nmax::Int64 = 1000, epsilon::Float64 = 1e-6, 
+        accumulate!::Function, accumulator::Array)
+    b = BTRDefaults()
+    state.fx = f(x0)
+    g!(x0, state.grad)
+    state.Δ = 0.1*norm(state.grad)
+    H!(x0, state.H)
+    
+    oldGrad = copy(state.grad)
+    y = zeros(length(x0))
+    while (!stop(state, nmax = nmax, tol = epsilon))
+        accumulate!(state, accumulator)
+        if verbose
+            println(state)
+        end
+        
+        state.step = TruncatedCG(state)
+        state.xcand = state.x+state.step
+        fcand = f(state.xcand)
+        state.ρ = (fcand-state.fx)/(dot(state.step, state.grad)+0.5*dot(state.step, state.H*state.step))
+        if acceptCandidate!(state, b)
+            state.x = copy(state.xcand)
+            oldGrad = copy(state.grad)
+            g!(state.x, state.grad)
+            y = state.grad - oldGrad
+            BFGS!(state.H, y, state.step)
+            state.fx = fcand
+        end
+        
+        updateRadius!(state, b)
+        state.it += 1
+    end
+    return state, accumulator
+end
+
 function OPTIM_btr_BFGS(f::Function, g!::Function, x0::Vector; verbose::Bool = true, nmax::Int64 = 1000, epsilon::Float64)
     H = Array{Float64, 2}(I, length(x0), length(x0))
-    
-    function accumulate!(state::BTRState{BFGS_Matrix}, acc::Vector)
-        push!(acc, state.fx)
-    end
-    accumulator = []
     state = BTRState(BFGS_Matrix(H))
     state.x = x0
     state.it = 0
     state.grad = zeros(length(x0))
     state, accumulator = btr(f, g!, state, x0,
-        verbose = verbose, nmax = nmax, epsilon = epsilon, accumulate! = accumulate!, accumulator = accumulator)
-    return state, accumulator
+        verbose = verbose, nmax = nmax, epsilon = epsilon, accumulate! = (st, acc) -> nothing, accumulator = [])
+    return state.x, state, accumulator
 end
--- src/BTR/BTR_True_Hessian.jl
+++ src/BTR/BTR_True_Hessian.jl
@@ -1,3 +1,37 @@
+function btr(f::Function, g!::Function, H!::Function, state::BTRState{Array{Float64,2}}, x0::Vector; 
+        verbose::Bool = true, nmax::Int64 = 1000, epsilon::Float64 = 1e-6, 
+        accumulate!::Function, accumulator::Array)
+    b = BTRDefaults()
+    state.fx = f(x0)
+    g!(x0, state.grad)
+    state.Δ = 0.1*norm(state.grad)
+    H!(x0, state.H)
+    
+    
+    while (!stop(state, nmax = nmax, tol = epsilon))
+        accumulate!(state, accumulator)
+        if verbose
+            println(state)
+        end
+        
+        state.step = TruncatedCG(state)
+        state.xcand = state.x+state.step
+        fcand = f(state.xcand)
+        state.ρ = (fcand-state.fx)/(dot(state.step, state.grad)+0.5*dot(state.step, state.H*state.step))
+        if acceptCandidate!(state, b)
+            state.x = copy(state.xcand)
+            g!(state.x, state.grad)
+            H!(state.x, state.H)
+            
+            state.fx = fcand
+        end
+        updateRadius!(state, b)
+        state.it += 1
+    end
+    return state, accumulator
+end
+
+
 function OPTIM_btr_TH(f::Function, g!::Function, H!::Function, 
                 x0::Vector; verbose::Bool = true, 
                 nmax::Int64 = 1000, epsilon::Float64 = 1e-4, 
